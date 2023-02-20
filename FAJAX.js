d4b7b6a296c4099e6e7e6c44d3e65b49314a48d5
da077 <da077@DESKTOP-HV19KNK>
2023-02-20 23:52:54 +0200

    callback

--- FAJAX.js
+++ FAJAX.js
@@ -7,8 +7,10 @@ class FXMLHttpRequest {
         this.responseText= null; //Returns a string that contains the response to the request as text, or null if the request was unsuccessful or has not yet been sent.
         this.status=''; //Returns the HTTP response status code of the request.
         this.network=new Network();
+        this.callBack='';
+
         this.addEventListener("abort",this.abort);
-        this.addEventListener("load",this.onload);
+       // this.addEventListener("load",this.onload);
     }
     open(method,url,async){
         this.method=method;
@@ -22,49 +24,30 @@ class FXMLHttpRequest {
 
     send(api,body=""){//get request
         let request="";
-        if(this.method==='POST'){
-            request=this.method+" /"+ url+"/api/" +api+" HTTP/1.1 "+'\n'+body;
-        }
-        else{
-            request= this.method+" /"+ url+"/api/"+api +" HTTP/1.1";
-        }
-        this.network.sendToServer(request);           
+        request=this.method+" /"+ url+"/api/" +api+" HTTP/1.1 "+'\n'+body;
+        let response= setTimeout(this.network.sendToServer(request),0);
+        responseFormat(response);
+        this.callBack();
+
     }
     abort(ev){//Aborts the request if it has already been sent.
 
     } 
 
+    onload(callBack){
+        this.callBack= callBack;
+    }
+
     //HTTP/1.1 200 OK '\n' body.json
-    onload(ev){
-        let response=ev; //?????????
-        // callback(null, response);
-        let req=response.split(' ');
-        this.status=req[1];
-        this.responseText=req[3].split('\n')[1];                   
-       //     callback(new Error('Request failed'), null);
-
+    responseFormat(response){
+        let res=response.split('\n');
+        this.status=(res[0].split(' '))[1];
+        this.responseText=res[1];                   
     }
 }
 
-/*
-function ready_status(stat){
-    if(stat===200){
-        return true;
-    }
-    else{
-        if(stat===404){
-            alert("sorry, the page not found");
-            return false;
-            //not found
-        }
-        if(stat===500){
-            alert("error cod");
-            return false;
-            //(Internal Server Error) response code
-        }
-    }
-}
-*/
+
+
 
 
 
